2d287a63de635e1baae5d74a10a166bcf761d415
namsuhan0322 <rnatpakfl@gmail.com>
2025-10-01 21:17:35 +0900

    [Feat] 5강 에코 궁 과제

--- Assets/Scripts/05/Echo.cs
+++ Assets/Scripts/05/Echo.cs
@@ -1,25 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Echo : MonoBehaviour
 {
     public float speed = 5f;
+    public float chronoBreakSpeed = 5f;
 
-    private Queue<Vector3> moveQueue;
-    private Stack<Vector3> moveHistory;
-    private GameObject gameObj;
-    private bool isMoving = false;
-    private Vector3 targetPos;
+    private Queue<(Vector3, bool)> _moveQueue = new Queue<(Vector3, bool)>();
+    private Stack<Vector3> _moveHistory = new Stack<Vector3>();
+
+    private Vector3 _targetPos;
+    private bool _isRecording = true;
+    private bool _isStack = false;
 
     void Start()
     {
-        moveQueue = new Queue<Vector3>();
-        moveHistory = new Stack<Vector3>();
-        gameObj = this.gameObject;
-        gameObj.GetComponent<Renderer>().material.color = Color.gray;
-        targetPos = transform.position;
+        _targetPos = transform.position;
+        gameObject.GetComponent<Renderer>().material.color = Color.gray;
+        _isRecording = true;
+        _moveQueue.Clear();
+        _moveHistory.Clear();
     }
 
     void Update()
@@ -27,48 +27,69 @@ public class Echo : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        if (!isMoving)
-        {
-            if (x != 0 || y != 0)
+        if (_isRecording)
+        {                    
+            if (x != 0 || y != 0 || Input.GetKeyDown(KeyCode.R))
             {
-                Vector3 move = new Vector3(x, y, 0).normalized * speed * Time.deltaTime;
-                targetPos += move; 
-                moveQueue.Enqueue(targetPos);
-                moveHistory.Push(targetPos);
+                Vector3 move = new Vector3(x, 0, y).normalized * speed * Time.deltaTime;
+                _targetPos += move;
+                _moveQueue.Enqueue((_targetPos, false));
             }
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.R))
+                RecordingStack();
+            else
+                _moveHistory.Push(_targetPos);
+
+            if (Input.GetKey(KeyCode.Space)) _isRecording = false;
+
+            return;
+        }
+
+        if (_moveQueue.Count > 0)
+        {
+            Vector3 target;
+            _isStack = false;
+            
+            (target, _isStack) = _moveQueue.Dequeue();
+            transform.position = target;
+
+            if (_isStack)
             {
-                if (!isMoving && moveQueue.Count > 0)
-                {
-                    isMoving = true;
-                    gameObj.GetComponent<Renderer>().material.color = Color.white;
-                }
+                gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            }
+            else
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.white;
             }
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            _isRecording = true;
+            _targetPos = transform.position; 
+            _moveHistory.Clear();
+
+            gameObject.GetComponent<Renderer>().material.color = Color.gray;
+        }
+    }
+
+    private void RecordingStack()
+    {
+        if (_moveHistory.Count > 0)
+        {
+            int historyCount = _moveHistory.Count;
+            for (int i = 0; i < historyCount; i++)
             {
-                if (moveHistory.Count > 1)
+                if (_moveHistory.Count == 0) break;
+
+                Vector3 target = _moveHistory.Pop();
+
+                if (i % chronoBreakSpeed == 0 || _moveHistory.Count == 0)
                 {
-                    targetPos = moveHistory.Pop();
-                    gameObj.GetComponent<Renderer>().material.color = Color.blue;
+                    _moveQueue.Enqueue((target, true));
+                    _targetPos = target;
                 }
             }
-            else
-            {
-                if (moveQueue.Count > 0)
-                {
-                    transform.position = moveQueue.Dequeue();
-                    isMoving = true;
-                }
-                else
-                {
-                    isMoving = false;
-                    gameObj.GetComponent<Renderer>().material.color = Color.gray;
-                }
-            }             
         }
     }
 }
